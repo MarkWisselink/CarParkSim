c20b9e2516d893ee30169b409fb24d633981c824
Ryan Flierman <r.w.flierman@st.hanze.nl>
2016-04-06 16:59:52 +0200

    Payment heeft nu ook ratePerQuarter

--- src/CarParkSim/objects/PaymentQueue.java
+++ src/CarParkSim/objects/PaymentQueue.java
@@ -17,7 +17,7 @@ package CarParkSim.objects;
 
 public class PaymentQueue extends CarQueue {
 
-    private double QuarterRate = 0.5; // default number. Method ChangeHourlyRate to change this number.
+    private double quarterRate = 0.5; // default number. Method ChangeHourlyRate to change this number.
     private double revenueToday = 0;
     private double reservationStartAmount = 3;
 
@@ -25,8 +25,8 @@ public class PaymentQueue extends CarQueue {
     }
 
     // get rate per hour
-    public double getHourlyRate(){
-        return hourlyRate;
+    public double getQuarterRate(){
+        return quarterRate;
     }
 
     // get the starting payment for reservations
@@ -35,29 +35,30 @@ public class PaymentQueue extends CarQueue {
     }
 
     // change the Start payment for reservations
-    public double changeHourlyRate(double StartAmountReservation){
+    public void setReservationStart(double StartAmountReservation){
         reservationStartAmount = StartAmountReservation;
     }
 
-    // change the rate per hour
-    public double changeHourlyRate(double RatePerHour) {
-        hourlyRate = RatePerHour;
-        return hourlyRate;
+    // change the rate per quarter
+    public void setQuarterRate(double RatePerQuarter) {
+        quarterRate = RatePerQuarter;
     }
 
 
     // calculates the amount of money that has to be paid to the Parking Garage
     public double PaymentAmount(Car car){
-        double moneyPayable = 0;
+        double moneyPayable;
+        int quarterAmount = (int) Math.ceil(car.getParkedTime() / 15);
+
 
         if(car instanceof ReservingCar){
-            moneyPayable = reservationStartAmount + (car.getParkedTime() * QuarterRate);
+            moneyPayable = reservationStartAmount + (quarterAmount  * quarterRate);
             revenueToday+= moneyPayable;
             return moneyPayable;
 
         } else{
 
-            moneyPayable= car.getParkedTime() * QuarterRate;
+            moneyPayable= quarterAmount * quarterRate;
             revenueToday+= moneyPayable;
             return moneyPayable;
         }
