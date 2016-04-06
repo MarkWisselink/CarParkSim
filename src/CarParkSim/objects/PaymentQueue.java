d4e4aea28055f78ad696aca72ad65624ed43ed1f
Ryan Flierman <r.w.flierman@st.hanze.nl>
2016-04-06 14:41:19 +0200

    payment dingen xx

---
+++ src/CarParkSim/objects/PaymentQueue.java
@@ -0,0 +1,24 @@
+package CarParkSim.objects;
+
+import java.util.* ;
+/**
+ *@author Nienke's boys
+ *
+ *
+ * BENODIGD:
+ * -  UURTARIEF
+ * - TIJD DIE EEN AUTO GEPARKEERD HEEFT
+ * - TOTALE REVENUE VAN DE DAG TOT NU TOE
+ * - REVENUE VAN DE AUTO'S DIE OP DIT MOMENT NOG IN DE GARAGE STAAN (DUS EX PASSHOLDERS)
+ * - PASSHOLDER JA/NEE -> DOORSTUREN NAAR EXIT
+ * -
+ *
+ */
+
+public class PaymentQueue extends CarQueue {
+
+    private double hourlyRate;
+
+
+}
+
