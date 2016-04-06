36b54201b9ae23b2ffcfc68ccc86fe4d458e90e2
Ryan Flierman <r.w.flierman@st.hanze.nl>
2016-04-06 15:27:55 +0200

    Payment uitgebreid

--- src/CarParkSim/objects/PaymentQueue.java
+++ src/CarParkSim/objects/PaymentQueue.java
@@ -1,12 +1,11 @@
 package CarParkSim.objects;
 
-import java.util.* ;
 /**
  *@author Nienke's boys
  *
  *
  * BENODIGD:
- * -  UURTARIEF
+ * -  UURTARIEF [check]
  * - TIJD DIE EEN AUTO GEPARKEERD HEEFT
  * - TOTALE REVENUE VAN DE DAG TOT NU TOE
  * - REVENUE VAN DE AUTO'S DIE OP DIT MOMENT NOG IN DE GARAGE STAAN (DUS EX PASSHOLDERS)
@@ -17,8 +16,37 @@ import java.util.* ;
 
 public class PaymentQueue extends CarQueue {
 
-    private double hourlyRate;
+    private double hourlyRate = 1.50; // default number. Method ChangeHourlyRate to change this number.
+    private double revenueToday = 0;
+   // private double RatePerhour;
 
+
+    // get rate per hour
+    public double getHourlyRate(){
+        return hourlyRate;
+    }
+
+    public double ChangeHourlyRate(double RatePerHour){
+        hourlyRate = RatePerHour;
+        return hourlyRate;
+    }
+
+    // calculates the amount of money that has to be paid to the Parking Garage
+    public double PaymentAmount(Car car){
+        double moneyPayable = 0;
+
+        if(car instanceof Passholders){
+            return moneyPayable;
+        } else{
+
+
+           // moneyPayable = hourlyRate * timeParked;
+            revenueToday+= moneyPayable;
+            return moneyPayable;
+        }
+
+    }
 
 }
+
 
