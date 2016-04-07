5c43e4f8b938dab3dc07ad7e7b192182f5cf0aff
Robert Zandberg <robert-zandberg@hotmail.com>
2016-04-07 11:42:57 +0200

    GUI part 1 added

---
+++ src/CarParkSim/view/GuiRobert.java
@@ -0,0 +1,48 @@
+package CarParkSim.view;
+import CarParkSim.logic.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Nienke's boys
+ */
+/**
+ *
+ * @author Robert
+ */
+public class GuiRobert extends AbstractView {
+
+    private JTextField fieldNumCars;
+    private JTextField fieldNumFloors;
+    private JTextField fieldTime;
+    
+    public GuiRobert(Model model) {
+        super(model);
+        //LEGE USER INTERFACE MET ALLEMAAL LEGE TEKSTBLOKKEN EN WEET IK VEEL WAT VOOR DINGEN JE ER ALLEMAAL IN WILT STOPPEN.
+        fieldNumCars = new JTextField();
+        fieldNumFloors = new JTextField();
+        fieldTime = new JTextField();
+        
+        //alle tekstvelden etc aanmaken
+    }
+
+    @Override
+    public void updateView() {
+        //tekstvelden updaten
+        int numCars = model.getNumberOfCars();
+        fieldNumCars.setText("Number of cars " + numCars + ".");
+        fieldNumCars.setEditable(false);        
+        int numFloors = model.getNumFloors();
+        fieldNumFloors.setText("Number of floors " + numFloors + ".");
+        fieldNumFloors.setEditable(false);
+        String time = model.getTime();
+        fieldTime.setText("Time :" + time + ".");
+        fieldTime.setEditable(false);
+        this.add(fieldNumCars);
+        this.add(fieldNumFloors);
+        this.add(fieldTime);
+        //Alle releveante info ophalen en die in de tekst velden stoppen. 
+        repaint();
+    }
+
+}
