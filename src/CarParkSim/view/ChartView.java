b1dc2ff245cfe72d8ecc8e5cb5ecd722ab5b73f1
Ryan Flierman <r.w.flierman@st.hanze.nl>
2016-04-12 12:47:49 +0200

    x

---
+++ src/CarParkSim/view/ChartView.java
@@ -0,0 +1,28 @@
+package CarParkSim.view;
+
+import java.awt.*;
+import java.lang.Object.*;
+
+import com.jjoe64.graphview.GraphView;
+import    com.jjoe64.graphview.series.BaseSeries<E>;
+import    com.jjoe64.graphview.series.BarGraphSeries<E>;
+
+/**
+ * Created by Nienke's boys on 12-4-2016.
+ */
+public class ChartView {
+
+    public ChartView(){
+    BarGraphSeries graph = new BarGraphSeries();
+        graph.draw(GraphView graphView, Canvas canvas)
+        )
+    }
+
+
+
+
+
+
+
+
+}
